37ec98eb2b8085986daacffc706ce6af67b787e1
joseramos1999 <jose1gatii@gmail.com>
2022-06-09 17:55:27 +0200

    8

--- ejercicios.js
+++ ejercicios.js
@@ -13,12 +13,22 @@ function ejercicio3() {
     let casado = true;
     const edad = 34;
 
-    console.log("Nombre: " + nombre + typeof(nombre) + " Casado: " + casado + typeof(casado) + " Edad: " + edad + typeof(edad));
+    console.log(
+        "Nombre: " +
+        nombre +
+        typeof nombre +
+        " Casado: " +
+        casado +
+        typeof casado +
+        " Edad: " +
+        edad +
+        typeof edad
+    );
 }
 
 const ejercicio4 = () => {
     return "Jose Ramos";
-}
+};
 
 console.log(ejercicio4);
 
@@ -29,8 +39,8 @@ function ejercicio4_2(comido) {
 ejercicio4_2(true);
 
 function ejercicio4_3(...num) {
-    num.forEach(Element => {
-        console.log(Element)
+    num.forEach((Element) => {
+        console.log(Element);
     });
 }
 
@@ -66,4 +76,67 @@ function ejercicio6_3(num1, num2) {
     }
 }
 
-function
+function ejercicio8() {
+    switch (mes) {
+        case 1:
+            console.log("Enero");
+            break;
+        case 2:
+            console.log("Febrero");
+            break;
+        case 3:
+            console.log("Marzo");
+            break;
+        case 4:
+            console.log("Abril");
+            break;
+        case 5:
+            console.log("Mayo");
+            break;
+        case 6:
+            console.log("Junio");
+            break;
+        case 7:
+            console.log("Julio");
+            break;
+        case 8:
+            console.log("Agosto");
+            break;
+        case 9:
+            console.log("Septiembre");
+            break;
+        case 10:
+            console.log("Octubre");
+            break;
+        case 11:
+            console.log("Noviembre");
+            break;
+        case 12:
+            console.log("Diciembre");
+            break;
+        default:
+            console.log("Tas equivocao primo");
+            break;
+    }
+
+    switch (numero) {
+        case "1":
+            return 1;
+            break;
+        case "2":
+            return 2;
+            break;
+        case "3":
+            return 3;
+            break;
+        case "4":
+            return 4;
+            break;
+        case "5":
+            return 5;
+            break;
+        default:
+            console.log("Tas equivocao primo");
+            break;
+    }
+}
